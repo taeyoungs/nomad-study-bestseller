c857318630bdb13b89fb1966b2d0f29804a38f96
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-25 16:28:09 +0900

    chore: 구글 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Dancing_Script, Raleway } from 'next/font/google';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const raleway = Raleway({ subsets: ['latin'] });
+const dacingScript = Dancing_Script({ subsets: ['latin'] });
 
 export const metadata = {
   description: 'New York Times Best Seller',
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${raleway.className} ${dacingScript.className}`}>{children}</body>
     </html>
   );
 }
