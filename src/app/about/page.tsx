19701fbd29372848d7c05a07f69e57e594e8eab6
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-25 20:44:08 +0900

    feat: About 페이지 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,23 @@
 import { css } from '../../../styled-system/css';
 
-export default function Home() {
+export default function About() {
   return (
-    <main>
-      <h3 className={css({ fontFamily: 'dancing', fontSize: '4xl', fontWeight: 'light' })}>
-        About
-      </h3>
-    </main>
+    <section className={css({ px: '40' })}>
+      <h2
+        className={css({
+          color: 'primary',
+          fontFamily: 'dancing',
+          fontSize: '5xl',
+          fontWeight: 'bold',
+          mb: '6',
+        })}
+      >
+        ABOUT US
+      </h2>
+      <p className={css({ fontFamily: 'raleway', fontSize: 'lg', mb: '2' })}>
+        Welcome to the official explorer for The New York Times Best Seller list explorer
+      </p>
+      <p className={css({ fontFamily: 'raleway', fontSize: 'lg' })}>We hope you enjoy your stay!</p>
+    </section>
   );
 }
