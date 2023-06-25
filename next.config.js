010fd8ef7e22bf1edbfaa2912d1f4376d18f5387
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-25 20:36:41 +0900

    chore: 외부 이미지 관련 next 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'storage.googleapis.com',
+        pathname: '/du-prd/books/images/**',
+        port: '',
+        protocol: 'https',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
